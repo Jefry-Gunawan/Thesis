4bb99f9bd95831dc32ed97884025431cfea79d6c
Jefry Gunawan <75528740+Jefry-Gunawan@users.noreply.github.com>
2024-01-30 14:00:55 +0700

    Initial Commit

---
+++ Thesis/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Thesis
+//
+//  Created by Jefry Gunawan on 30/01/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
